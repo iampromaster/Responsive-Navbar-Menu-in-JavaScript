1060458dbafb32a742de13b8e3dd8cd89466a0e7
pypymi <dr.mizoguchi@gmail.com>
2024-06-09 12:37:02 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.right = "0"
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.right = "-100%";
+}
+
+
